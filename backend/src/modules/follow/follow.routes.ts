ff05285d7c451359b43353624ffb60e8dd70e0dd
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-04-28 21:20:24 +0300

    feat: добавл ratelimit на кнопки подписки/отписки

--- backend/src/modules/follow/follow.routes.ts
+++ backend/src/modules/follow/follow.routes.ts
@@ -5,14 +5,23 @@ import { ProfileService } from '../profile/profile.service.js'
 import { withUserMiddleware } from '@/middlewares/user.middleware.js'
 import { FollowController } from './follow.controller.js'
 import { authMiddleware } from '@/middlewares/auth.middleware.js'
+import rateLimit from 'express-rate-limit'
 
 const router = express.Router()
 const c = new FollowController(
   new FollowService(new UserService(new ProfileService()))
 )
 
+const followLimit = rateLimit({
+  windowMs: 10 * 1000,
+  limit: 5,
+  message: {
+    message: "Ну что ты тыкаешь, тыкаешь, решись уже — подписан ты или нет)"
+  }
+})
+
 router.get('/follows/status/:userId', withUserMiddleware, c.getFollowingStatus)
-router.post('/follows/follow', authMiddleware, c.follow)
-router.post('/follows/unfollow', authMiddleware, c.unfollow)
+router.post('/follows/follow', followLimit, authMiddleware, c.follow)
+router.post('/follows/unfollow',followLimit, authMiddleware, c.unfollow)
 
 export default router
--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -3,58 +3,72 @@ import axios, { type InternalAxiosRequestConfig, type AxiosError, type AxiosResp
 import { API_URL } from "../config/env";
 import { ApiError } from "../lib/api";
 import type { ApiErrorType } from "../lib/api/api-error";
+import { toast } from "sonner";
 
 interface AxiosConfigWithRetry extends InternalAxiosRequestConfig {
-  _isRetry?: boolean;
+	_isRetry?: boolean;
 }
 interface AxiosErrorWithRetry extends AxiosError {
-  config: AxiosConfigWithRetry;
-  response: AxiosResponse<ApiErrorType>;
+	config: AxiosConfigWithRetry;
+	response: AxiosResponse<ApiErrorType>;
 }
 
 const instance = axios.create({
-  withCredentials: true,
-  baseURL: API_URL,
-  headers: {
-    "Content-Type": "Application/json",
-  },
+	withCredentials: true,
+	baseURL: API_URL,
+	headers: {
+		"Content-Type": "Application/json",
+	},
 });
 export const api = instance;
 
 instance.interceptors.response.use(
-  (res) => {
-    return res;
-  },
-  async (error: AxiosErrorWithRetry) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && error.config && !error.config._isRetry) {
-      originalRequest._isRetry = true;
-      try {
-        originalRequest._isRetry = false;
-        await api.post("/auth/refresh");
-        return api(originalRequest);
-      } catch {
-        throw new ApiError(error.response?.data.message ?? "Неизвестная ошибка", error.response?.status, {
-          message: error.response?.data.message,
-          errors: error.response?.data.errors,
-        });
-      }
-    }
+	(res) => {
+		return res;
+	},
+	async (error: AxiosErrorWithRetry) => {
+		const originalRequest = error.config;
+		const status = error.response?.status ?? error.status ?? 500;
+		const data = error.response?.data;
+		if (error.response?.status === 401 && error.config && !error.config._isRetry) {
+			originalRequest._isRetry = true;
+			try {
+				await api.post("/auth/refresh");
+				return api(originalRequest);
+			} catch {
+				throw new ApiError(
+					error.response?.data.message ?? "Неизвестная ошибка",
+					error.response?.status,
+					{
+						message: error.response?.data.message,
+						errors: error.response?.data.errors,
+					},
+				);
+			}
+		}
+		const GLOBAL_ERRORS: Partial<Record<number, string>> = {
+			429: data?.message ?? "Слишком много запросов, попробуйте позже.",
+			500: "Ошибка сервера",
+		};
 
-    const networkErrorMessage = "Ошибка подключения к сети";
-    const backendErrorMessage = error.response.data.message;
-    const status = error.response?.status ?? error.status ?? 500;
+		if (GLOBAL_ERRORS[status]) {
+			console.log(GLOBAL_ERRORS[status]);
+			console.log(GLOBAL_ERRORS);
+			console.log(status);
 
-    let message = "Неизвестная ошибка";
-    if (backendErrorMessage) {
-      message = backendErrorMessage;
-    } else if (error.code === "ERR_NETWORK") {
-      message = networkErrorMessage;
-    }
+			toast.error(GLOBAL_ERRORS[status]);
+		}
 
-    throw new ApiError(message, status, {
-      message: message,
-      errors: error.response?.data.errors,
-    });
-  },
+		let message = "Неизвестная ошибка";
+		if (data?.message) {
+			message = data.message;
+		} else if (error.code === "ERR_NETWORK") {
+			message = "Ошибка подключения к сети";
+		}
+
+		throw new ApiError(message, status, {
+			message: message,
+			errors: data?.errors,
+		});
+	},
 );
