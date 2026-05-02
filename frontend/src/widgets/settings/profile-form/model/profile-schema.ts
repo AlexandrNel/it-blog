f55cb3b107d144f0e68cbfbd724281bbc29e0d03
Alexandr Nelyubov <sashanelyubov2005@gmail.com>
2026-05-02 20:51:41 +0300

    fix: привел к общей zod схеме

--- frontend/src/widgets/settings/profile-form/model/profile-schema.ts
+++ frontend/src/widgets/settings/profile-form/model/profile-schema.ts
@@ -1,34 +1,39 @@
 import z from "zod";
 
 export const profileSettingsSchema = z.object({
-  displayName: z
-    .string()
-    .min(1, "Минимум 1 символ")
-    .max(32, { message: "Максимум 32 символа" })
-    .nullable()
-    .optional()
-    .or(z.literal("")),
-  bio: z.string().max(200, { message: "Максимум 200 символов" }).optional().or(z.literal("")),
-  location: z.string().optional(),
-  contacts: z
-    .object({
-      email: z.email().optional().or(z.literal("")),
-      site: z.url().optional().or(z.literal("")),
-      links: z
-        .object({
-          github: z
-            .url({ hostname: /github/ })
-            .optional()
-            .or(z.literal("")),
-          telegram: z
-            .url({ hostname: /telegram/ })
-            .optional()
-            .or(z.literal("")),
-        })
-        .optional(),
-    })
-    .nullable()
-    .optional(),
+	displayName: z
+		.string()
+		.min(1, "Минимум 1 символ")
+		.max(32, { message: "Максимум 32 символа" })
+		.nullable()
+		.optional()
+		.or(z.literal("")),
+	bio: z.string().max(200, { message: "Максимум 200 символов" }).optional().or(z.literal("")),
+	location: z.string().optional(),
+	contacts: z
+		.object({
+			email: z.email().optional().or(z.literal("")),
+			site: z.url().optional().or(z.literal("")),
+			links: z
+				.object({
+					github: z
+						.url({ hostname: /github/ })
+						.optional()
+						.or(z.literal("")),
+					telegram: z
+						.string()
+						.optional()
+						.or(z.literal(""))
+						.refine((val) => {
+							if (val) {
+								return /^https?:\/\/(t\.me|telegram\.me)\/[a-zA-Z0-9_]{5,32}$/.test(val);
+							}
+						}),
+				})
+				.optional(),
+		})
+		.nullable()
+		.optional(),
 });
 
 export type ProfileSettingsFormValues = z.infer<typeof profileSettingsSchema>;
